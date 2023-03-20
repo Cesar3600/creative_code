79d3136632d20322a9625d02e84c89eb396224fb
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-03-19 22:56:43 -0500

    add logic to squares

--- sketch-01.js
+++ sketch-01.js
@@ -6,8 +6,38 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'orange';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    quantity= 7
+    border=100
+    gap = 30
+    squareVal = ((width+gap-(border*2))/quantity )-gap
+    pad = squareVal*0.1
+
+    for (let i = 0; i < quantity; i++) {
+      for (let j = 0; j < quantity; j++) {
+        context.beginPath()
+        let x = border+(squareVal+gap)*i
+        let y = border+(squareVal+gap)*j
+        context.rect(x,y,squareVal,squareVal)
+        context.stroke()
+
+        if(Math.random() > 0.5){
+           
+          let bIntX = squareVal-(pad*2)
+          let bTintY = squareVal-(pad*2)
+          context.beginPath()
+          context.rect(x+pad, y+pad,bIntX,bTintY)
+          context.stroke()
+        }
+      }
+    }
+
+
+
+
+
   };
 };
 
